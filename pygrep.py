1687c14006855f0f2737b446451ccec001a2adfa
josewdf <60181010+josemlwdf@users.noreply.github.com>
2024-04-09 21:15:34 +0200

    Delete pygrep.py

--- pygrep.py
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# This Bash script executes a Python3 command to perform a text search within a file using Python's capabilities
-# It resolves situations where 'grep' returns an error like 'grep: yourfile: binary file matches'
-# Usage: pygrep [string] [file name]
-# Example: pygrep 'search_string' 'file_to_search.txt'
-python -c "with open('$2', 'r') as file: [print(line.strip()) for line in file if '$1' in line]"
