55fd6df7e064ed773a8b94a61929b89cdea5daee
josewdf <60181010+josemlwdf@users.noreply.github.com>
2024-11-07 13:50:41 +0100

    Update custom_wsl.sh

--- custom_wsl.sh
+++ custom_wsl.sh
@@ -68,7 +68,7 @@ echo 'deb-src http://http.kali.org/kali kali-rolling main non-free contrib' | su
 curl -s https://raw.githubusercontent.com/josemlwdf/CTFEnum/main/install.sh | bash
 
 # Install various packages
-sudo apt install net-tools steghide freerdp2-x11 responder mitmproxy stegsnow cupp openvpn mariadb-client-core ffuf pipx file php exiftool impacket-scripts rlwrap john smbmap smbclient nikto exploitdb hydra wpscan poppler-utils sqlmap hash-identifier enum4linux hashcat dos2unix whatweb docker.io knockd evil-winrm jq ltrace sntp tftp-hpa -y
+sudo apt install net-tools steghide freerdp2-x11 responder mitmproxy stegsnow cupp openvpn unrar mariadb-client-core ffuf pipx file php exiftool impacket-scripts rlwrap john smbmap smbclient nikto exploitdb hydra wpscan poppler-utils sqlmap hash-identifier enum4linux hashcat dos2unix whatweb docker.io knockd evil-winrm jq ltrace sntp tftp-hpa -y
 pipx ensurepath
 sudo pip install requests git-dumper pyftpdlib --break-system-packages
 
