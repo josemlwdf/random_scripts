c96dc4f3236c2a06c80fa03d534307cbaecd2180
josewdf <60181010+josemlwdf@users.noreply.github.com>
2025-10-20 10:57:56 +0200

    Add inbound firewall rule for WSL port forwarding
    Added inbound firewall rule for port forwarding.

--- portforward.ps1
+++ portforward.ps1
@@ -1,6 +1,6 @@
 $ports = @(21,25,22,445,80,443,4444,5555,6666,7777,8000,8080,8888,9999,11601);
 
-$wslAddress = wsl bash -c "ifconfig eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'"
+$wslAddress = wsl bash -c "ip a show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'"
 
 if ($wslAddress -match '^(\d{1,3}\.){3}\d{1,3}$') {
   Write-Host "WSL IP address: $wslAddress" -ForegroundColor Green
@@ -24,4 +24,5 @@ foreach ($port in $ports) {
 
 Invoke-Expression "Remove-NetFireWallRule -DisplayName $fireWallDisplayName -ErrorAction SilentlyContinue";
 Invoke-Expression "New-NetFireWallRule -DisplayName $fireWallDisplayName -Direction Outbound -LocalPort $portsStr -Action Allow -Protocol TCP";
+
 Invoke-Expression "New-NetFireWallRule -DisplayName $fireWallDisplayName -Direction Inbound -LocalPort $portsStr -Action Allow -Protocol TCP";
