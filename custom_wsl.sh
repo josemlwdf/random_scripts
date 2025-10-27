7bc6652b8e2e739c48c3c433fe8b78762fd3f9fc
josewdf <60181010+josemlwdf@users.noreply.github.com>
2025-10-27 09:27:11 +0100

    Update custom_wsl.sh

--- custom_wsl.sh
+++ custom_wsl.sh
@@ -104,6 +104,7 @@ sudo /usr/bin/curl -s https://github.com/josemlwdf/random_scripts/raw/refs/heads
 sudo /usr/bin/curl -s https://github.com/josemlwdf/random_scripts/raw/refs/heads/main/Inveigh.exe -o Inveigh.exe 2>/dev/null
 sudo /usr/bin/curl -s https://raw.githubusercontent.com/josemlwdf/random_scripts/refs/heads/main/Inveigh.ps1 -o Inveigh.ps1 2>/dev/null
 sudo /usr/bin/curl -s https://raw.githubusercontent.com/josemlwdf/DomainPasswordSpray/refs/heads/master/DomainPasswordSpray.ps1 -o DomainPasswordSpray.ps1 2>/dev/null
+sudo /usr/bin/curl -s https://github.com/josemlwdf/random_scripts/raw/refs/heads/main/WMIC.exe -o WMIC.exe 2>/dev/null
 
 # Install various packages
 PKGS=(
@@ -253,6 +254,7 @@ $(drv=G; mountpoint="/mnt/$(echo $drv | tr '[:upper:]' '[:lower:]')"; echo "$drv
 # Update locate database
 echo updating file database
 sudo updatedb
+
 
 
 
